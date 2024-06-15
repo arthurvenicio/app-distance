6cac7986518e11e6cd4a5d114475cf370f3fc592
arthurvenicio <arthur@justcode.dev>
2024-06-15 13:57:47 -0300

    testing printer

--- app/src/main/java/com/example/appdistance/MainActivity.kt
+++ app/src/main/java/com/example/appdistance/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.appdistance
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.appdistance.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//       binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val printTextWithoutScreenButton: Button = findViewById(R.id.button_calculate)
+
+        printTextWithoutScreenButton.setOnClickListener {
+            print("true", fromResource(R.raw.printable_text))
+        }
+    }
+
+    private fun print(showFeedback: String, stringRaw: String) {
+        val deeplinkReturnScheme = getString(R.string.scheme_return)
+
+        val uriBuilder = Uri.Builder()
+        uriBuilder.authority("print")
+        uriBuilder.scheme("printer-app")
+        uriBuilder.appendQueryParameter("SHOW_FEEDBACK_SCREEN", showFeedback)
+        uriBuilder.appendQueryParameter("SCHEME_RETURN", deeplinkReturnScheme)
+        uriBuilder.appendQueryParameter("PRINTABLE_CONTENT", stringRaw)
+        startMyActivity(uriBuilder)
+    }
+
+    private fun startMyActivity(uriBuilder: Uri.Builder) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.data = uriBuilder.build()
+        startActivity(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null && intent.data != null) {
+            Log.d("TESTE", intent.data.toString())
+            Toast.makeText(this, intent.data.toString(), Toast.LENGTH_LONG).show()
         }
     }
 }
